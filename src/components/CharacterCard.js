fa04b5b8b72c48625898da3788639b803150146d
jessica <fuerte.jessica10@gmail.com>
2021-12-10 02:03:08 -0600

    cant figure out how to do dropdown

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,30 +4,34 @@ import styled from 'styled-components';
 
 
 const StyledCard = styled.div`
-border: 5px solid;
+
 width: 100%;
 padding-top: 10px;
 `
 const StyledName = styled.div`
 font-size:40px;
 color: #432f21;
+text-align: center;
 `
 const StyledTop = styled.div`
+
 `
 const StyledBottom = styled.div`
+
 `
 const StyledWhole = styled.div`
 display: flex;
 text-align: center;
-justify-content: center
+justify-content: center;
+color: black;
+font-size: 30px;
 `
 
 
  const CharacterCard = ({data}) => {
      return (
     <StyledCard className='card'>
-        
-       <StyledName>{data.name}</StyledName>
+        <StyledName>{data.name}</StyledName>
        <StyledWhole>
        <StyledTop>
         <p> Gender: {data.gender}   </p>
@@ -38,7 +42,6 @@ justify-content: center
         <StyledBottom>
         <p> Eye Color: {data.eye_color}</p>
         <p> Hair Color: {data.hair_color}</p>
-        <p> Mass: {data.mass}</p>
         <p> Skin Color: {data.skin_color}</p>
         </StyledBottom>
         </StyledWhole>
